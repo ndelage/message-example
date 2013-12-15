2a5de78602791242b086eeb3226e43bde1698562
Nate Delage <nate@natedelage.com>
2013-12-15 13:37:47 -0600

    Initial Commit. Sample Ruby code with Javi

---
+++ example.rb
@@ -0,0 +1,42 @@
+class Message
+  attr_accessor :message
+
+  def initialize(msg)
+    @log = []
+
+    @config = {:message => msg,
+               :cursive => false,
+               :backup_messages => ["goodbye", "bonjour"]}
+  end
+
+  def print_message?(time)
+    true
+  end
+
+  def print
+    if print_message?(Time.now)
+      @log << get_message
+
+      puts get_message
+    end
+  end
+
+  def display_log
+    puts "#{@log.size}: "
+    puts @log.inspect
+  end
+
+  def get_message
+    if rand(10) > 5
+      @config[:message]
+    else
+      @config[:backup_messages].sample
+    end
+  end
+end
+
+m = Message.new("hello world")
+
+m.print
+m.print
+m.display_log
